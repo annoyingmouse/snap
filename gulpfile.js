58f326ab20f3841c5b5724a6a1a16dcccdd3bd7c
Dominic Myers <annoyingmouse@gmail.com>
2023-03-14 17:06:23 +0000

    Updated after Dependabot complained.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass");
-const babel = require("gulp-babel");
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const { watch } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
 
 // style paths
-const
-    sassFiles = "assets/styles/sass/**/*.scss",
-    cssDest = "assets/styles/css",
-    es6Files = "assets/scripts/es6/**/*.*",
-    jsDest = "assets/scripts/js";
-
-gulp.task("styles", () => gulp.src(sassFiles)
-    .pipe(sass().on("error", sass.logError))
+const sassFiles = "assets/styles/sass/**/*.scss",
+      cssDest = "assets/styles/css",
+      es6Files = "assets/scripts/es6/**/*.*",
+      jsDest = "assets/scripts/js";
+const buildStyles = cb => {
+  gulp.src(sassFiles)
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest(cssDest))
-);
+  cb()
+}
+const es6 = cb => {
+  gulp.src(es6Files)
+      .pipe(babel({presets: ['@babel/preset-env']}))
+      .pipe(gulp.dest(jsDest))
+  cb()
+}
+gulp.task('buildStyles', async () => buildStyles)
 
-gulp.task("es6", () => gulp.src(es6Files)
-    .pipe(babel())
-    .pipe(gulp.dest(jsDest)));
+gulp.task('es6', async () => es6)
 
+exports.default = function() {
+  watch(sassFiles, buildStyles)
+  watch(es6Files, es6)
+}
 
-gulp.task("watch", () => {
-    gulp.watch(sassFiles, ["styles"]);
-    gulp.watch(es6Files, ["es6"]);
-});
